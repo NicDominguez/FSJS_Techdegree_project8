3fdc545684fb76f25e2c0da6f911bed5ab232c12
NicDominguez <ndomingu22@gmail.com>
2020-02-26 14:12:39 -0800

    created search feature and error handlelling

--- app.js
+++ app.js
@@ -9,17 +9,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/static', express.static('public'))
+
 app.use(routes);
 
-(async () => {
-  await db.sequelize.sync();
+// Page Not Found Error Throw
+app.use((req, res, next) => {
+  const error = new Error('Page Not Found')
+  error.status = 404
+  next(error)
+})
 
-  try {
-    /* await sequelize.authenticate();
-        console.log('Connection to the database successful'); */
-  } catch (error) {
-    /* console.error('Error connecting to the database: ', error) */
+// Error Handler
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.locals.error = err;
+  res.status(err.status);
+  
+  if (err.status === 404) {
+    res.render('page_not_found')
+  } else {
+    res.render('error');
   }
-})();
+  
+})
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const db = require("../db");
 const { Book } = db.models;
 
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -11,22 +13,76 @@ function asyncHandler(cb){
         try {
             await cb(req, res, next)
         } catch (error) {
-            res.status(500).send(error);
+            if (error.name === "SequelizeValidationError") {
+                res.render('form_error') 
+            } else {
+                return next(error)
+            }
         }
     }
 }
+
+// Pagination Helper Function
+const paginate = (query, { page, pageSize }) => {
+    const offset = page * pageSize;
+    const limit = pageSize;
+  
+    return {
+      ...query,
+      offset,
+      limit,
+    };
+  };
+  
+  
 
 // Routes
 
 router.get('/', (req, res) => {
     res.redirect('/books');
-
 });
 
 router.get('/books', asyncHandler(async (req, res) => {
     const allBooks = await Book.findAll()
-    res.render('all_books', {allBooks});
+    if (allBooks) {
+        res.render('all_books', {allBooks});
+    } else {
+        throw error = {
+            status: 500,
+            message: "I'm sorry but it looks like the application had trouble accessing the library"
+        };
+    }
 }));
+
+router.post('/books', asyncHandler(async (req, res) => {
+    const searchText = req.body.bookSearch.toLowerCase()
+    const searchedBooks = await Book.findAndCountAll({
+        where: {
+            [Op.or]: {
+                title: {
+                    [Op.like]: `%${searchText}%`
+                },
+                author: {
+                    [Op.like]: `%${searchText}%`
+                },
+                genre: {
+                    [Op.like]: `%${searchText}%`
+                },
+                year: {
+                    [Op.like]: `%${searchText}%`
+                }
+            }
+        }
+    })
+    if (searchedBooks.count > 0) {
+        res.render('all_books', {searchedBooks});
+    } else {
+        throw error = {
+            status: 500,
+            message: "I'm soory, but your search did not turn up any results"
+        }
+    }
+}))
 
 router.get('/books/new', asyncHandler(async (req, res) => {
     res.render('new_book');
@@ -34,7 +90,15 @@ router.get('/books/new', asyncHandler(async (req, res) => {
 
 router.get('/books/:id', asyncHandler(async (req, res) => {
     const singleBook = await Book.findByPk(req.params.id);
-    res.render('book_detail', {singleBook});
+    if (singleBook) {
+        res.render('book_detail', {singleBook});
+    } else {
+        throw error = {
+            status: 500,
+            message: "I'm sorry but that book does not exist in our database"
+        };
+    }
+        
 }));
 
 
@@ -47,7 +111,7 @@ router.post('/books/new', asyncHandler(async (req, res) => {
 router.post('/books/:id', asyncHandler(async(req, res) => {
     const bookToUpdate = await Book.findByPk(req.params.id);
     await bookToUpdate.update(req.body);
-    res.redirect('/books' + bookToUpdate.id);
+    res.redirect('/books/' + bookToUpdate.id);
 }));
 
 router.post('/books/:id/delete', asyncHandler(async(req, res) => {
